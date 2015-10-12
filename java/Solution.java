299e4494f452a9cbd221942d019179ce27a74b66
Vishal Gaurav <vishal.gaurav@hotmail.com>
2015-10-12 00:56:40 -0400

    Commit for question 1st and 2nd of Hacker rank on Linked list
    Commit for question 1st and 2nd of Hacker rank on Linked list

---
+++ java/Solution.java
@@ -0,0 +1,37 @@
+
+public class Solution {
+  /*
+    Node is defined as 
+    class Node {
+       int data;
+       Node next;
+    }
+ */
+ /**
+ * Print the elements of a linked list
+ * #HackerRank
+ */
+    void Print(Node head) {
+        if (head != null) {
+            while (head != null) {
+                System.out.println(head.data);
+                head = head.next;
+            }
+        }
+    }
+/**
+ * Insert a node at the tail of a linked list
+ * #HackerRank
+ */
+    Node Insert(Node head, int data) {
+        Node headNode = head;
+        while (head.next != null) {
+            head = head.next;
+        }
+        Node lastNode = new Node();
+        lastNode.data = data;
+        lastNode.next = null;
+        head.next = lastNode;
+        return headNode;
+    }
+}
