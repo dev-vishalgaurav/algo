4f3b054a34d79ba0d6e502a15874a0ecfc697fb8
Vishal Gaurav <vishal.gaurav@hotmail.com>
2015-10-19 00:19:38 -0400

    Added method for Selection Sort
    Added method for selection sort and a test method to check if array is
    sorted.

--- sorting/Sorting.java
+++ sorting/Sorting.java
@@ -3,15 +3,35 @@ package algos;
  * 
  * @author Vishal Gaurav , vishal.gaurav@hotmail.com
  * This file contains implementation for few sorting algorithms
- *
+ * sorting using generic objects 
  */
-public class Sorting<T> {
+public class Sorting<T extends Comparable<T>> {
 
     public static void main(String[] args) {
-        Integer[] arrayToBeSorted = getTestArray(); 
+        Integer[] arrayToBeSorted = getTestArray();
+        new Sorting<Integer>().SelectionSort(arrayToBeSorted, arrayToBeSorted.length);
+        System.out.println(arrayToBeSorted);
+        boolean isSorted = isArraySorted(arrayToBeSorted);
+        System.out.println("sorting success = " + isSorted);
     }
     
-    
+    /**
+     * method to verify sorting of an array.
+     * @param arrayToBeSorted
+     * @return true is array passed is sorted.
+     */
+    private static boolean isArraySorted(Integer[] arrayToBeSorted) {
+        boolean result = true;
+        for (int i = 0; i < arrayToBeSorted.length-1; i++) {
+            if(arrayToBeSorted[i].compareTo(arrayToBeSorted[i+1]) > 0){
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+
     private static Integer[] getTestArray() {
         Integer[] testArray = new Integer[35];
         for (int i = 0; i < testArray.length; i++) {
@@ -27,7 +47,18 @@ public class Sorting<T> {
      * @param length
      */
     public void SelectionSort(T[] arrayToBeSorted, int length ){
-        
+        int minIndex ;
+         for (int count = 0; count < arrayToBeSorted.length; count++) {
+               minIndex = count ;
+            for (int countInner = count; countInner < arrayToBeSorted.length; countInner++) {
+                if(arrayToBeSorted[countInner].compareTo(arrayToBeSorted[minIndex]) <= 0 ){
+                    minIndex = countInner ;
+                }
+            }
+            T tempValue = arrayToBeSorted[count];
+            arrayToBeSorted[count] = arrayToBeSorted[minIndex];
+            arrayToBeSorted[minIndex] = tempValue;
+        }
     }
     /**
      * this method will sort an array by BubbleSort algorithm
