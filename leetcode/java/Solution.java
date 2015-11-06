5a6f3985d0b40a1fb2b4918154adda59434ece8a
Vishal Gaurav <vishal.gaurav@hotmail.com>
2015-11-05 21:03:53 -0500

    Leetcode problem connect all the leaf nodes.
    connect all the leaf nodes.
    Populate each next pointer to point to its next right node. If there is
    no next right node, the next pointer should be set to NULL.

--- leetcode/java/Solution.java
+++ leetcode/java/Solution.java
@@ -236,4 +236,175 @@ public class Solution {
         }
         return result;
     }
+
+/** 
+ *#LeetCode <br></br>
+ * https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
+ * https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+ * This method is a solution for LeetCode problem 1 and 2 which asks to
+ * connect all the leaf nodes. 
+ * <br>
+ * <b> Space complexity O(n) </b>
+ * </br>
+ * <br>
+ * <b> Time complexity O(n) </b>
+ * </br>
+ * Following is the description of the question
+ * <div class="question-content">
+ * <p>
+ * </p>
+ * <p>
+ * Given a binary tree
+ * </p>
+ * 
+ * <pre>
+ *     struct TreeLinkNode {
+      TreeLinkNode *left;
+      TreeLinkNode *right;
+      TreeLinkNode *next;
+    }
+ * </pre>
+ * <p>
+ * </p>
+ * 
+ * <p>
+ * Populate each next pointer to point to its next right node. If there is
+ * no next right node, the next pointer should be set to <code>NULL</code>.
+ * </p>
+ * 
+ * <p>
+ * Initially, all next pointers are set to <code>NULL</code>.
+ * </p>
+ * 
+ * <p>
+ * <b>Note:</b>
+ * </p>
+ * <ul>
+ * <li>You may only use constant extra space.</li>
+ * <li>You may assume that it is a perfect binary tree (ie, all leaves are
+ * at the same level, and every parent has two children).</li>
+ * </ul>
+ * <p>
+ * </p>
+ * 
+ * <p>
+ * For example,<br>
+ * Given the following perfect binary tree,<br>
+ * </p>
+ * 
+ * <pre>
+ *          1
+       /  \
+      2    3
+     / \  / \
+    4  5  6  7
+ * </pre>
+ * <p>
+ * </p>
+ * <p>
+ * After calling your function, the tree should look like:<br>
+ * </p>
+ * 
+ * <pre>
+ *          1 -&gt; NULL
+       /  \
+      2 -&gt; 3 -&gt; NULL
+     / \  / \
+    4-&gt;5-&gt;6-&gt;7 -&gt; NULL
+ * </pre>
+ * <p>
+ * </p>
+ * <p>
+ * </p>
+ * 
+ * <div>
+ * <p>
+ * <a href="/subscribe/">Subscribe</a> to see which companies asked this
+ * question
+ * </p>
+ * </div>
+ * 
+ * 
+ * 
+ * <div> <div id="tags" class="btn btn-xs btn-warning">Show Tags</div>
+ * <span class="hidebutton">
+ * 
+ * <a class="btn btn-xs btn-primary" href="/tag/tree/">Tree</a>
+ * 
+ * <a class=
+ * "btn btn-xs btn-primary" href="/tag/depth-first-search/">Depth-first
+ * Search</a>
+ * 
+ * </span> </div>
+ * 
+ * 
+ * 
+ * <div> <div id="similar" class="btn btn-xs btn-warning">Show Similar
+ * Problems</div> <span class="hidebutton">
+ * 
+ * <a class="btn btn-xs btn-primary" href=
+ * "/problems/populating-next-right-pointers-in-each-node-ii/"> (H)
+ * Populating Next Right Pointers in Each Node II</a>
+ * 
+ * <a class=
+ * "btn btn-xs btn-primary" href="/problems/binary-tree-right-side-view/">
+ * (M) Binary Tree Right Side View</a>
+ * 
+ * </span> </div>
+ * </div>
+ * 
+ * @param rootNode
+ */
+    public void connect(TreeLinkNode rootNode) {
+        if (rootNode != null) {
+            List<BstNode> queue = new ArrayList<>();
+            BstNode root = new BstNode();
+            root.node = rootNode;
+            root.level = 0;
+            queue.add(root);
+            BstNode prevLevelNode = null;
+            while (queue.size() > 0) {
+                BstNode nextNode = queue.remove(0); // pop from queue
+                if (nextNode.node.left != null) {
+                    BstNode left = new BstNode();
+                     left.node = nextNode.node.left;
+                     left.level = nextNode.level + 1;
+                    queue.add(left);
+                }
+                if (nextNode.node.right != null) {
+                    BstNode right = new BstNode();
+                     right.node = nextNode.node.right;
+                     right.level = nextNode.level + 1;
+                    queue.add(right);
+                }
+                
+                if(prevLevelNode != null && (prevLevelNode.level == nextNode.level)){
+                    prevLevelNode.node.next = nextNode.node;
+                }
+                prevLevelNode = nextNode;
+
+            }
+        }
+    }
+/**
+ * used by connect method.
+ *
+ */
+    public class TreeLinkNode {
+        int val;
+        TreeLinkNode left, right, next;
+        TreeLinkNode(int x) {
+            val = x;
+        }
+    }
+/**
+ * 
+ * 
+ *wrapper around TreeLinkNode to maintain level order.
+ */
+    private static class BstNode {
+        TreeLinkNode node;
+        int level;
+    }
+
 }
