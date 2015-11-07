a253f94d6fcc99dd819da7ddb9828a088b7d751e
Vishal Gaurav <vishal.gaurav@hotmail.com>
2015-11-07 03:01:06 -0500

    Leetcode delete a node question.
    # https://leetcode.com/problems/delete-node-in-a-linked-list/
    Write a function to delete a node (except the tail) in a singly linked
    list,  given only access to that node. Supposed the linked list is 1 ->
    2 -> 3 -> 4 and you are given
    the third node with value 3,the linked list should become 1 -> 2 -> 4
    after calling your function.

--- leetcode/java/Solution.java
+++ leetcode/java/Solution.java
@@ -461,4 +461,16 @@ public class Solution {
         }
         return nums.length - totalFound;
     }
+/**
+*Leetcode
+*Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+*Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
+*https://leetcode.com/problems/delete-node-in-a-linked-list/
+*/
+	public void deleteNode(ListNode node) {
+      if(node.next!=null){
+          node.val = node.next.val;
+          node.next=node.next.next;
+      }
+    }
 }
