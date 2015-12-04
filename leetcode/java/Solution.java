44c82208d01412ec2df114c34961f14298822c8b
Vishal Gaurav <vishal.gaurav@hotmail.com>
2015-12-04 09:04:17 -0500

    Leetcode height balanced binary tree problem.
    Leetcode height balanced binary tree problem.

--- leetcode/java/Solution.java
+++ leetcode/java/Solution.java
@@ -675,4 +675,30 @@ public class Solution {
 	private int min(int first, int second){
         return (first < second) ? first : second ;
     }
+	
+	/**
+ * Leetcode height balanced binary tree problem. 
+ * Given a binary tree, determine if it is height-balanced.
+ * For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the 
+ * two subtrees of every node never differ by more than 1.
+ * https://leetcode.com/problems/balanced-binary-tree/
+ */
+    public boolean isBalanced(TreeNode root) {
+        boolean result = true;
+        if(root == null){
+            result = true;   
+        }else if(root.left == null && root.right == null){
+            result = true;
+        }else if(root.right != null && root.left == null){
+            result = getHeight(root.right) == 1;
+        }else if(root.left != null && root.right == null){
+            result = getHeight(root.left) == 1;
+        }else{
+            result =  (Math.abs(getHeight(root.left) - getHeight(root.right)) <=1) && (isBalanced(root.left) && isBalanced(root.right)) ;
+        }
+        return result;
+    } 
+    public int getHeight(TreeNode root){
+        return (root == null) ? 0 : max(getHeight(root.left),getHeight(root.right)) + 1 ; 
+    }
 }
