d71a79df6cdf8ae6f30e04efe3fcf8096b492604
Vishal Gaurav <vishal.gaurav@hotmail.com>
2016-06-27 00:40:36 -0400

    updating efficient solution for Leetcode sort-transformed-array problem

--- leetcode/java/sort-transformed-array/Solution.java
+++ leetcode/java/sort-transformed-array/Solution.java
@@ -1,9 +1,59 @@
+/**
+ * https://leetcode.com/problems/sort-transformed-array/
+ */
 public class Solution {
-    /**
-	* https://leetcode.com/problems/sort-transformed-array/
-	* Naive solution not O(n)
-	*/
+
+	/**
+	 * Efficient solution inspired from this link https://leetcode.com/discuss/108831/java-o-n-incredibly-short-yet-easy-to-understand-ac-solution
+	 * 
+	 */
+	 public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
+        int[] result = new int[nums.length];
+        int i = 0, j = nums.length-1, startIndex = (a >= 0) ? nums.length - 1 : 0;
+        while(i <= j){
+            int sqrsi = getVal(nums[i],a,b,c);
+            int sqrsj = getVal(nums[j],a,b,c);
+            if(a >= 0){
+                if(sqrsi >= sqrsj){
+                    result[startIndex--] = sqrsi;
+                    i++;
+                }else{
+                    result[startIndex--] = sqrsj;
+                    j--;
+                }
+            }else{
+                if(sqrsi >= sqrsj){
+                    result[startIndex++] = sqrsj;
+                    j--;
+                }else{
+                    result[startIndex++] = sqrsi;
+                    i++;
+                }
+            }
+        }
+        return result;
+    }
+	/**
+	 * Shorter version of the above implementation
+	 */
 	public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
+		int[] result = new int[nums.length];
+		int[] sqrs = new int[nums.length];
+		int i = 0, j = nums.length-1, startIndex = (a >= 0) ? nums.length - 1 : 0;
+		while(i <= j){
+			sqrs[i] = (sqrs[i] == 0 ) ? getVal(nums[i],a,b,c) : sqrs[i] ;
+			sqrs[j] = (sqrs[j] == 0 ) ? getVal(nums[j],a,b,c) : sqrs[j];
+			if(a>=0)
+				result[startIndex--] = sqrs[i] >= sqrs[j] ? sqrs[i++] : sqrs[j--]; // sorting from last
+			else
+				result[startIndex++] = sqrs[i] >= sqrs[j] ? sqrs[j--] : sqrs[i++]; // sorting from begining
+		}
+		return result;
+	}
+	/**
+	 * Naive Solution 
+	 */
+    public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
         int[] result = new int[nums.length];
         for(int i = 0 ; i < nums.length ; i++){
             result[i] = getVal(nums[i],a,b,c);
@@ -19,6 +69,6 @@ public class Solution {
         nums[to] = temp;
     }
     private int getVal(int x, int a , int b, int c){
-        return (a*x*x) + b*x + c;
+        return a*x*x + b*x + c;
     }
 }
