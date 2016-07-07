7983ec9380a34ff9ce7bc629787ca8c45f26fc81
Vishal Gaurav <vishal.gaurav@hotmail.com>
2016-07-07 02:38:18 -0400

    Adding solution for Leetcode perfect-squares problem

---
+++ leetcode/java/perfect-squares/Solution.java
@@ -0,0 +1,15 @@
+https://leetcode.com/problems/perfect-squares/
+public class Solution {
+    public int numSquares(int n) {
+        if(n <= 0) return 0;
+        int[] dp = new int[n+1];
+        for(int i = 1 ; i <= n ; i++){
+            int min = Integer.MAX_VALUE;
+            for(int j = 1 ; j*j <= i ; j++){
+                min = Math.min(min,dp[i - j*j] + 1);
+            }
+            dp[i] = min;
+        }
+        return dp[n];
+    }
+}
