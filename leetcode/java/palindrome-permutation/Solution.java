9a7ca5adad2efe9f7895e549e2b77c5ca0e6abd1
Vishal Gaurav <vishal.gaurav@hotmail.com>
2016-07-08 02:04:57 -0400

    adding solution for Leetcode palindrome-permutation problem

---
+++ leetcode/java/palindrome-permutation/Solution.java
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/palindrome-permutation/
+ */
+public class Solution {
+    public boolean canPermutePalindrome(String s) {
+        int odds = 0;
+        int count[] = new int[256];
+        for(int i = 0 ; i < s.length() ; i++){
+            char c = s.charAt(i);
+            int index = 
+            count[c]++;
+            odds = (count[c] % 2 == 0 ) ? odds -1 : odds + 1;
+        }
+        return odds <=1;
+    }
+}
